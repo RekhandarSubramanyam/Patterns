e09d983779e680d141c531bd66a81497c9b90140
Subramanyam Rekhandar <53898299+RekhandarSubramanyam@users.noreply.github.com>
2022-01-24 11:22:45 +0530

    Update patern.py

--- patern.py
+++ patern.py
@@ -3,3 +3,4 @@ def print_rangoli(size):
     alpha = string.ascii_lowercase
     for i in list(range(size))[::-1] + list(range(1, size)):
         print('-'.join(alpha[size-1:i:-1] + alpha[i:size]).center(width, '-'))
+        print'-'.join(alpha[size-1:i:-1] + alpha[i:size]).center(width, '-'))
